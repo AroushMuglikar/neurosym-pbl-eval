72b53909d2b4c5f322151b29311ced0dabc1aa66
aroush <aroushmuglikar@gmail.com>
2026-09-14 20:58:24 +0530

    Update run_pipeline_dummy.py

--- scripts/run_pipeline_dummy.py
+++ scripts/run_pipeline_dummy.py
@@ -1,1 +1,6 @@
+# Main execution script to test the neuro-symbolic pipeline
+# DO NOT RUN until data loaders are finished and raw data is in /data/
 
+if __name__ == "__main__":
+    print("Neuro-Symbolic Pipeline initialized.")
+    print("Awaiting data ingestion module to begin episodic buffering.")
