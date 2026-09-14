6d2b5d241d87778025ea44e8206aa0c8ceb51465
aroush <aroushmuglikar@gmail.com>
2026-09-14 21:23:19 +0530

    Update __init__.py

--- src/encoders/__init__.py
+++ src/encoders/__init__.py
@@ -1,1 +1,7 @@
-
+"""
+Multimodal Perception Layer
+---------------------------
+Wrappers for early-fusion and late-fusion neural feature extraction.
+Targeting physiological arousal (PPG/Eye-tracking via SOPHIAS) and 
+static code complexity metrics.
+"""
