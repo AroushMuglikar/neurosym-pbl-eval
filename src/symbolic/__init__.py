e70c4b16f7e2e6a623b4ab9d68f1511d5532f903
aroush <aroushmuglikar@gmail.com>
2026-09-14 21:22:53 +0530

    Update __init__.py

--- src/symbolic/__init__.py
+++ src/symbolic/__init__.py
@@ -1,1 +1,8 @@
-
+"""
+Symbolic Alignment Engine (SAE)
+-------------------------------
+The deterministic guardrail layer. Maps continuous neural embeddings 
+from the Perception Layer onto discrete, human-interpretable rubrics.
+Prevents LLM hallucination by enforcing hard logical bounds on feature 
+spaces prior to final grade aggregation.
+"""
